211ddcb0e33242d2dd28e3290b62020730df76eb
glaurios <adrienglaurios@gmail.com>
2025-07-29 16:55:24 +0000

    Done

---
+++ app/api/inngest/User/Data/route.js
@@ -0,0 +1,12 @@
+
+
+
+export async function GET(request) {
+    
+    try{
+
+    }catch (error) {
+        console.error("Error in GET request:", error);
+        return new Response("Internal Server Error", { status: 500 });
+    }
+}
